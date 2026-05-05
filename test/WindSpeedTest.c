ed2ac1ad79cba683cd3dc6bb0ab44dc3b3ca4757
Benjamin K. Hansen <bkh2604@gmail.com>
2026-05-05 10:18:08 +0200

    Bugfix/WindSpeed setup is now ready

--- test/WindSpeedTest.c
+++ test/WindSpeedTest.c
@@ -1,8 +1,18 @@
 #include <unity.h>
 #include "WindSpeed.h"
+#include "SharedWeather.h"
+
+void handlePinChange(uint8_t pins);
+
+static void simulateWindClick(void)
+{
+    handlePinChange((1 << PK0) | (1 << PK1));
+    handlePinChange(1 << PK0);
+}
 
 void setUp() {
-
+    ws_clicks = 0;
+    ws_last = 1;
 }
 
 void tearDown() {
