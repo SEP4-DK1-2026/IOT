82f5d5be6456ac968aebf3dfabdeca388f401fb5
PoelseJonas <jonas.schwarz@hotmail.dk>
2026-05-07 12:56:09 +0200

    Update: Fjernet udgået metoder

--- lib/drivers/WindDir.h
+++ lib/drivers/WindDir.h
@@ -5,10 +5,6 @@
 
 void WindDir_init(void);
 uint16_t WindDir_getADC(void);
-uint16_t WindDir_getCachedADC(void);
-const char *WindDir_getText(void);
 uint16_t WindDir_getDeg(void);
 void WindDir_resetCache(void); 
-
-
 #endif
