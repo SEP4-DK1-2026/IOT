547a0181637aaa2b50389913d6c77493a7143d94
Benjamin K. Hansen <bkh2604@gmail.com>
2026-05-08 11:08:19 +0200

    Bugfix/Minimized the data transfer to be under the limit

--- src/network.c
+++ src/network.c
@@ -66,8 +66,8 @@ void send_sensor_data(sensor_data_t *data)
     char rain_num[10];
     char wind_speed_num[10];
 
-    dtostrf(data->rain, 6, 2, rain_num);
-    dtostrf(data->wind_speed, 6, 2, wind_speed_num);
+    dtostrf(data->rain, 1, 2, rain_num);
+    dtostrf(data->wind_speed, 1, 2, wind_speed_num);
     sprintf(json,
             "{\"temp\":%d.%d,\"hum\":%d.%d,\"light\":%d, \"rain\":%s, \"wspeed\":%s, \"wdir\":%d}",
             data->temp_i, data->temp_d,
